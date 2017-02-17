5257193829e49b2798f58cea8c7b84321f7c648d
Matt Wiens <mwiens91@gmail.com>
2017-02-17 15:16:27 -0800

    More work on image processing

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 A Python3 script to read data from https://www.youtube.com/watch?v=raiFrxbHxV0
+Writes to a CSV spreadsheet
 
 Use:
 
@@ -7,5 +8,8 @@ python3 redBullFetch.py videoname
 ```
 
 Works on Linux, probably on Mac, and definitely not on Windows.
-Has quite a few dependencies, too, which I'll list if I get around
-to it.
+
+Dependencies:
+
+tesserocr - https://pypi.python.org/pypi/tesserocr
+Pillow	  - https://pypi.python.org/pypi/Pillow/4.0.0
--- redBullFetch.py
+++ redBullFetch.py
@@ -1,43 +1,65 @@
 import os
 import subprocess
+import csv
 from PIL import Image
 from tesserocr import image_to_text
 
-def splitVideo(avFile, interval=200 ,saveDir=os.getcwd() + '/screens'):
-    # interval is in milliseconds - convert to fps
+def generateScreenCaps(avFile, interval=200
+        ,screenDir=os.getcwd() + '/screens'):
+    # Interval is in milliseconds - convert to fps
     fps = '%f' % (10**3 / interval)
 
-    # check if save directory exists. create it if not
-    if not os.path.exists(saveDir):
-        os.makedirs(saveDir)
+    # Check if save directory exists; create it if not
+    if not os.path.exists(screenDir):
+        os.makedirs(screenDir)
 
-    # generate screencaptures
+    # Generate screencaptures ussing ffmpeg
     subprocess.run(['ffmpeg', '-i', './' + avFile, '-vf', 'fps=' + fps,
-        saveDir + '/img%05d.bmp']) 
+        screenDir + '/img%05d.bmp']) 
 
     return
 
 def processScreenCap(screenCaptureObj):
 
-    timeImg = screenCaptureObj.crop((589,659,750,698))
-    altitudeImg =                   # in m 
-    speedImg =                      # in kph 
-    heartImg =                      # in bpm
-    respirImg =                     # respiration in ???
+    # Extract subset of image corresponding to data
+    timeImg = screenCaptureObj.crop((585,662,749,698))
+    altitudeImg = screenCaptureObj.crop((1359,226,1464,254))    # in m
+    speedImg = screenCaptureObj.crop((1665,226,1764,256))       # in kph 
+    heartImg = screenCaptureObj.crop((1368,526,1460,555))       # in bpm
+    respirImg = screenCaptureObj.crop((1356,565,1463,594))      # respiration
+                                                                # in ????
 
+    # Extract text from images using Tesseract OCR
     time = image_to_text(timeImg)
+    altitude = image_to_text(altitudeImg)
+    speed = image_to_text(speedImg)
+    heart = image_to_text(heartImg)
+    respir = image_to_text(respirImg)
 
+    # Strip trailing whitespace Tesseract OCR seems to pick up 
+    time = time.rstrip()
+    altitude = altitude.rstrip()
+    speed = speed.rstrip()
+    heart = heart.rstrip()
+    respir = respir.rstrip()
 
+    return (time, altitude, speed, heart, respir)
+
+def parseScreenCaptures(csvFile, screenDir=os.getcwd() + '/screens'):
+    
 
 if __name__ == '__main__':
     import sys
 
-#    videoPath = sys.argv[1]
+    videoPath = sys.argv[1]
 
-#    print('Generating frames ...')
-#    splitVideo(videoPath)
-#    print('Done ...')
+    print("Generating screencaptures . . .")
+    generateScreenCaps(videoPath)
+    print("Done")
+
+    print("Writing to CSV . . . ")
+
 
     screen = Image.open('./screens/img000001.bmp')
 
-    processScreenCap(screen)
+    print(processScreenCap(screen))
